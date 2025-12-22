18e249f2262d519e375ee5ed8c1e54a6561c5f57
Vaibhav Singh <its0519vaibhavsingh@gmail.com>
2025-12-22 10:48:01 +0530

    Time: 6 ms (100%), Space: 44.4 MB (37.18%) - LeetHub

---
+++ 0960-delete-columns-to-make-sorted-iii/0960-delete-columns-to-make-sorted-iii.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int minDeletionSize(String[] A) {
+        int rows = A.length;
+        int cols = A[0].length();
+        
+        int[] dp = new int[cols];
+        int max = 1;
+
+        // Each column alone is valid
+        for (int j = 0; j < cols; j++) {
+            dp[j] = 1;
+        }
+
+        for (int j = 0; j < cols; j++) {
+            for (int i = 0; i < j; i++) {
+                if (isValid(A, i, j, rows)) {
+                    dp[j] = Math.max(dp[j], dp[i] + 1);
+                }
+            }
+            max = Math.max(max, dp[j]);
+        }
+
+        return cols - max;
+    }
+
+    private boolean isValid(String[] A, int i, int j, int rows) {
+        for (int r = 0; r < rows; r++) {
+            if (A[r].charAt(i) > A[r].charAt(j)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
