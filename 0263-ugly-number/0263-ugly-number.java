0f537d61ba80058ff84df8108e38705446bf196d
Vaibhav Singh <its0519vaibhavsingh@gmail.com>
2025-12-29 19:08:30 +0530

    Time: 1 ms (98.17%), Space: 42.6 MB (47.75%) - LeetHub

---
+++ 0263-ugly-number/0263-ugly-number.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean isUgly(int n) {
+        if (n<=0) return false;
+        
+        for ( int p:new int[]{2,3,5})
+            while (n%p ==0) n/=p;
+        return n==1;
+    }
+}
