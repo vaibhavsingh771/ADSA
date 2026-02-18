32f3c5394ae4fa0a89f949084930b3bd00d3880d
Vaibhav Singh <its0519vaibhavsingh@gmail.com>
2026-02-18 10:25:19 +0530

    Time: 490 ms (64.05%), Space: 0B (100%) - LeetHub

--- 1075-project-employees-i/1075-project-employees-i.sql
+++ 1075-project-employees-i/1075-project-employees-i.sql
@@ -1,5 +1,4 @@
-SELECT project_id,
-    ROUND(AVG(experience_years), 2) AS average_years
+SELECT project_id, ROUND(AVG(experience_years), 2) AS average_years
 FROM Project p JOIN Employee e
 ON p.employee_id = e.employee_id
 GROUP BY project_id
