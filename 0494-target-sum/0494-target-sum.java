20d40730005cced110241529d1b9dbbbc69787a3
Vaibhav Singh <its0519vaibhavsingh@gmail.com>
2026-05-03 22:03:17 +0530

    Time: 2 ms (99.89%), Space: 43 MB (75.55%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,18 +1,24 @@
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
         int sum = 0;
-        for (int n : nums) sum += n;
 
-        if (sum < Math.abs(target) || (sum + target) % 2 != 0) return 0;
+        for (int num : nums) {
+            sum += num;
+        }
+        if ((sum + target) % 2 != 0 || Math.abs(target) > sum) {
+            return 0;
+        }
 
-        int s = (sum + target) / 2;
-        int[] dp = new int[s + 1];
+        int subsetSum = (sum + target) / 2;
+        int[] dp = new int[subsetSum + 1];
         dp[0] = 1;
 
-        for (int n : nums)
-            for (int i = s; i >= n; i--)
-                dp[i] += dp[i - n];
+        for (int num : nums) {
+            for (int j = subsetSum; j >= num; j--) {
+                dp[j] += dp[j - num];
+            }
+        }
 
-        return dp[s];
+        return dp[subsetSum];
     }
 }
