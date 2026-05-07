a201b545bc415b7b58285ddb8cba5b18e21c1548
Vaibhav Singh <its0519vaibhavsingh@gmail.com>
2026-05-07 12:07:19 +0530

    Time: 1580 ms (5.03%), Space: 0B (100%) - LeetHub

--- 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
+++ 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
@@ -7,12 +7,3 @@ select product_id,ifnull(
     order by change_date desc limit 1
 ),10) AS price from products p1
 group by product_id;
-
--- SELECT product_id, IFNULL(
---         (SELECT new_price FROM Products p2
---         WHERE p1.product_id = p2.product_id
---             AND change_date <= '2019-08-16'
---         ORDER BY change_date DESC LIMIT 1
---         ),10) AS price
--- FROM Products p1
--- GROUP BY product_id;
