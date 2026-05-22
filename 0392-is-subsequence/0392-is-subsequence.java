65ea13279e16c31d97ac395ed2b3c51f6774ce27
Vaibhav Singh <its0519vaibhavsingh@gmail.com>
2026-05-22 10:30:11 +0530

    Time: 1 ms (94.63%), Space: 43.8 MB (5.68%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.java
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -3,12 +3,12 @@ class Solution {
         return helper(s, t, 0, 0);
     }
 
-    private boolean helper(String s, String t, int i, int j) {
+    boolean helper(String s, String t, int i, int j) {
         if (i == s.length()) return true;
         if (j == t.length()) return false;
         if (s.charAt(i) == t.charAt(j)) {
             return helper(s, t, i + 1, j + 1);
-        }
-        return helper(s, t, i, j + 1);
+        }else
+            return helper(s, t, i, j + 1);
     }
 }
